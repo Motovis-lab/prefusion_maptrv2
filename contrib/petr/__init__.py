4eaffc010b7e3e37158c7837498707223ca93160
rlan <brianlanbo@gmail.com>
2024-09-18 11:00:13 +0800

    added cp_fpn and model.

--- contrib/petr/__init__.py
+++ contrib/petr/__init__.py
@@ -1,3 +1,4 @@
 from .model import *
 from .metrics import *
 from .tensor_smith import *
+from .cp_fpn import CPFPN
--- contrib/petr/model.py
+++ contrib/petr/model.py
@@ -15,3 +15,27 @@ class ToyModel(BaseModel):
 
     def forward(self, x, mode='loss'):
         return self.head(self.backbone.layer0(x))
+
+
+
+
+
+@MODELS.register_module()
+class StreamPETR(BaseModel):
+    def __init__(self, *, backbone=None, neck=None, 
+                #  box_head=None, 
+        **kwargs):
+        super().__init__()
+        assert not any(m is None for m in [backbone, neck])
+        self.backbone = MODELS.build(backbone)
+        self.neck = MODELS.build(neck)
+        # self.box_head = MODELS.build(box_head)
+    
+    def forward(self, model_food, mode='loss'):
+        if mode=='loss':
+            return self.forward_train(model_food)
+        else:
+            return self.forward_test(model_food)
+    
+    def forward_train(self, model_food):
+        pass
