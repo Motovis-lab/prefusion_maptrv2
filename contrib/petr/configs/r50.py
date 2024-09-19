37ad545d5f1bdec7759b508257b6d73165952daf
rlan <brianlanbo@gmail.com>
2024-09-19 18:50:03 +0800

    added focal_head and locations.

--- contrib/petr/configs/r50.py
+++ contrib/petr/configs/r50.py
@@ -1,15 +1,12 @@
 experiment_name = "stream_petr_r50_demo"
 
-_base_ = '../../../configs/default_runtime.py'
+_base_ = "../../../configs/default_runtime.py"
 
 # custom_imports = dict(
 #     imports=['models', 'datasets', 'hooks', 'runner', 'utils', 'evaluator', 'losses'],
 #     allow_failed_imports=False
 # )
-custom_imports = dict(
-    imports=['prefusion', 'contrib'],
-    allow_failed_imports=False
-)
+custom_imports = dict(imports=["prefusion", "contrib"], allow_failed_imports=False)
 # custom_imports = dict(
 #     imports=['prefusion', 'contrib'],
 #     allow_failed_imports=False
@@ -17,23 +14,27 @@ custom_imports = dict(
 
 backend_args = None
 
+det_classes = [
+    "class.vehicle.passenger_car",
+    "class.traffic_facility.box",
+    "class.road_marker.arrow",
+    "class.parking.text_icon",
+    "class.cycle.motorcycle",
+]
+
 train_dataloader = dict(
     num_workers=1,
     persistent_workers=True,
-    sampler=dict(type='DefaultSampler'),
-    collate_fn=dict(type='collate_dict'),
+    sampler=dict(type="DefaultSampler"),
+    collate_fn=dict(type="collate_dict"),
     dataset=dict(
         type="GroupBatchDataset",
-        name='MvParkingTest',
-        data_root='/Users/rlan/work/dataset/motovis/mv4d',
-        info_path='/Users/rlan/work/dataset/motovis/mv4d/mv4d_infos.pkl',
+        name="MvParkingTest",
+        data_root="/Users/rlan/work/dataset/motovis/mv4d",
+        info_path="/Users/rlan/work/dataset/motovis/mv4d/mv4d_infos.pkl",
         dictionaries={
             "camera_images": {},
-            "bbox_3d": {
-                "det": {
-                    "classes": ['class.vehicle.passenger_car', 'class.traffic_facility.box', 'class.road_marker.arrow', 'class.parking.text_icon', 'class.cycle.motorcycle']
-                }
-            },
+            "bbox_3d": {"det": {"classes": det_classes}},
         },
         tensor_smiths=dict(
             camera_images=dict(
@@ -44,61 +45,52 @@ train_dataloader = dict(
             bbox_3d=dict(type="Bbox3DCorners"),
         ),
         model_feeder=dict(type="StreamPETRModelFeeder"),
-        transformable_keys=[
-            'camera_images', 'bbox_3d'
-        ],
+        transformable_keys=["camera_images", "bbox_3d"],
         transforms=[
-            dict(
-                type="RandomMirrorSpace",
-                prob=0.5,
-                scope='group'
-            ),
+            dict(type="RandomMirrorSpace", prob=0.5, scope="group"),
             dict(
                 type="RandomImageISP",
                 prob=0.5,
             ),
         ],
-        phase='train',
+        phase="train",
         batch_size=2,
         possible_group_sizes=[3, 4, 5],
-        possible_frame_intervals=[1, 2]
-    )
+        possible_frame_intervals=[1, 2],
+    ),
 )
 
 val_dataloader = train_dataloader
 test_dataloader = train_dataloader
 
-train_cfg = dict(type='GroupBatchTrainLoop', max_epochs=24, val_interval=-1)  # -1 note don't eval
-val_cfg = dict(type='GroupValLoop')
-test_cfg = dict(type='GroupTestLoop')
+train_cfg = dict(type="GroupBatchTrainLoop", max_epochs=24, val_interval=-1)  # -1 note don't eval
+val_cfg = dict(type="GroupValLoop")
+test_cfg = dict(type="GroupTestLoop")
 
 model = dict(
-    type='StreamPETR',
+    type="StreamPETR",
     data_preprocessor=dict(
         type="FrameBatchMerger",
         device="mps",
     ),
     img_backbone=dict(
-        pretrained='torchvision://resnet50',
-        type='mmdet.ResNet',
+        pretrained="torchvision://resnet50",
+        type="mmdet.ResNet",
         depth=50,
         num_stages=4,
         out_indices=(2, 3),
         frozen_stages=-1,
-        norm_cfg=dict(type='BN2d', requires_grad=False),
+        norm_cfg=dict(type="BN2d", requires_grad=False),
         norm_eval=True,
         with_cp=True,
-        style='pytorch'
+        style="pytorch",
     ),
-    img_neck=dict(
-        type='mmdet3d.CPFPN',
-        in_channels=[1024, 2048],
-        out_channels=256,
-        num_outs=2
+    img_neck=dict(type="mmdet3d.CPFPN", in_channels=[1024, 2048], out_channels=256, num_outs=2),
+    roi_head=dict(
+        type="FocalHead",
+        num_classes=len(det_classes),
     ),
-    box_head=dict(
-        type='StreamPETRHead'
-    ),
+    box_head=dict(type="StreamPETRHead"),
 )
 
 val_evaluator = dict(type="Accuracy")
@@ -107,31 +99,30 @@ test_evaluator = dict(type="Accuracy")
 
 env_cfg = dict(
     cudnn_benchmark=False,
-    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
-    dist_cfg=dict(backend='nccl'),
+    mp_cfg=dict(mp_start_method="fork", opencv_num_threads=0),
+    dist_cfg=dict(backend="nccl"),
 )
 
-lr = 0.006  # total lr per gpu lr is lr/n 
+lr = 0.006  # total lr per gpu lr is lr/n
 
 optim_wrapper = dict(
-    type='OptimWrapper',
-    optimizer=dict(type='AdamW', lr=lr, weight_decay=0.01),
-    clip_grad=dict(max_norm=35, norm_type=2))
-
-param_scheduler = dict(type='MultiStepLR', milestones=[16, 20])
-
-default_hooks = dict(
-    timer=dict(type='IterTimerHook'),
-    logger=dict(type='LoggerHook', interval=50),
-    param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', interval=1, save_best='precision', rule='greater'),
-    sampler_seed=dict(type='DistSamplerSeedHook')
+    type="OptimWrapper",
+    optimizer=dict(type="AdamW", lr=lr, weight_decay=0.01),
+    clip_grad=dict(max_norm=35, norm_type=2),
 )
 
-visualizer = dict(type='Visualizer',
-                  vis_backends=[dict(type='LocalVisBackend'),
-                                dict(type='TensorboardVisBackend')])
+param_scheduler = dict(type="MultiStepLR", milestones=[16, 20])
+
+default_hooks = dict(
+    timer=dict(type="IterTimerHook"),
+    logger=dict(type="LoggerHook", interval=50),
+    param_scheduler=dict(type="ParamSchedulerHook"),
+    checkpoint=dict(type="CheckpointHook", interval=1, save_best="precision", rule="greater"),
+    sampler_seed=dict(type="DistSamplerSeedHook"),
+)
+
+visualizer = dict(type="Visualizer", vis_backends=[dict(type="LocalVisBackend"), dict(type="TensorboardVisBackend")])
 
 
 # load_from = "work_dirs/mv_4d_fastbev_t/20240903_023804/epoch_24.pth"
-resume=False
+resume = False
--- contrib/petr/model.py
+++ contrib/petr/model.py
@@ -34,12 +34,43 @@ class FrameBatchMerger(BaseDataPreprocessor):
 
 @MODELS.register_module()
 class StreamPETR(BaseModel):
-    def __init__(self, *, data_preprocessor=None, img_backbone=None, img_neck=None, box_head=None, stride=16, **kwargs):
+    def __init__(
+        self,
+        *,
+        data_preprocessor=None,
+        img_backbone=None,
+        img_neck=None,
+        roi_head=None,
+        box_head=None,
+        stride=16,
+        position_level=0,
+        **kwargs
+    ):
+        """_summary_
+
+        Parameters
+        ----------
+        data_preprocessor : _type_, optional
+            _description_, by default None
+        img_backbone : _type_, optional
+            _description_, by default None
+        img_neck : _type_, optional
+            _description_, by default None
+        box_head : _type_, optional
+            _description_, by default None
+        stride : int, optional
+            _description_, by default 16
+        position_level : int, optional
+            用于选择 FPN 结果中的哪一个粒度的特征来做后续的 bbox 预测, by default 0
+        """
         super().__init__(data_preprocessor=data_preprocessor)
         assert not any(m is None for m in [img_backbone, img_neck])
         self.img_backbone = MODELS.build(img_backbone)
         # self.box_head = MODELS.build(box_head)
+        self.roi_head = MODELS.build(roi_head)
         self.img_neck = MODELS.build(img_neck) if img_neck else None
+        self.stride = stride
+        self.position_level = position_level
 
     def forward(self, *args, mode="loss", **kwargs):
         if mode == "loss":
@@ -50,11 +81,15 @@ class StreamPETR(BaseModel):
     def forward_train(self, *, index_info=None, camera_images=None, bbox_3d=None, meta_info=None):
         B, (N, C, H, W) = len(camera_images), camera_images[0].shape
         camera_images = torch.vstack([i.unsqueeze(0) for i in camera_images]).reshape(B * N, C, H, W)
+        im_size = camera_images.shape[-2:][::-1]
         img_feats = self.extract_img_feat(camera_images)
         if self.img_neck is not None:
             img_feats = self.img_neck(img_feats)
+        img_feats = img_feats[self.position_level]
+
+        location = self.prepare_location(img_feats, B, N, im_size)
+        outs_roi = self.roi_head(location, )
         
-        location = self.prepare_location(img_feats)
 
     def forward_test(self, *args, **kwargs):
         return
@@ -67,12 +102,10 @@ class StreamPETR(BaseModel):
             of shape (N, C, H, W), where N is the batch size, C is usually 3, H and W are image height and width.
         """
         return self.img_backbone(img)
-    
-    def prepare_location(self, img_feats, img_size):
+
+    def prepare_location(self, img_feats, batch_size, num_cameras, img_size):
         pad_w, pad_h = img_size
-        bs, n = img_feats.shape[:2]
-        x = img_feats.flatten(0, 1)
-        location = locations(x, self.stride, pad_h, pad_w)[None].repeat(bs*n, 1, 1, 1)
+        location = locations(img_feats, self.stride, pad_h, pad_w)[None].repeat(batch_size * num_cameras, 1, 1, 1)
         return location
 
 
