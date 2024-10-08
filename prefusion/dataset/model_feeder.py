9349c438e1024fbe62c9949f1cc0a490003c8b71
AlphaLFC <398505136@qq.com>
2024-10-08 10:13:20 +0800

    update model_feeder for fastray

--- prefusion/dataset/model_feeder.py
+++ prefusion/dataset/model_feeder.py
@@ -1,11 +1,8 @@
-import torch
-import numpy as np
-
 
 from prefusion.registry import MODEL_FEEDERS
 
 
-__all__ = ["BaseModelFeeder", "FastRayFastBEVFeeders"]
+__all__ = ["BaseModelFeeder"]
 
 
 @MODEL_FEEDERS.register_module()
@@ -48,61 +45,3 @@ class BaseModelFeeder:
         
         return processed_group_batch
 
-
-
-
-@MODEL_FEEDERS.register_module()
-class FastRayFastBEVFeeders(BaseModelFeeder):
-
-    def __init__(self, 
-                 camera_feature_configs: dict,
-                 voxel_feature_config: dict, 
-                 bbox_3d_config: dict,
-                 polyline_3d_config: dict,
-                 phase: str = 'train'):
-        pass
-
-
-    def process(self, frame_batch: list) -> dict | list:
-        """
-        Parameters
-        ----------
-        frame_batch : list
-            list of input_dicts
-        training : bool, optional
-            _description_, by default False
-
-        Returns
-        -------
-        dict | list
-            _description_
-
-        Notes
-        -----
-        ```
-        input_dict = {
-            'scene_id': scene_id,
-            'frame_id': frame_id,
-            'prev_exists': True,
-            'next_exists': True,
-            'transformables': {}
-        }
-        batch_input_dict ={
-            'scene_ids': [scene_id, scene_id, ...],
-            'frame_ids': [frame_id, frame_id, ...],
-            'prev_exists': [True, True, ...],
-            'next_exists': [True, True, ...],
-            'transformables': {
-                'lidar_points': [lidar_points, lidar_points, ...],
-                'camera_image_batches': {
-                    cam_id: (N, 3, H, W),
-                    cam_id: (N, 3, H, W)
-                }
-            }
-            'LUTS': [LUT, LUT, ...],
-        }
-        ```
-        """
-        for input_dict in frame_batch:
-            input_dict['transformables']['camera_image_set'].to_tensor()
-    
