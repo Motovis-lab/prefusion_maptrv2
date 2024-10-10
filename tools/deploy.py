6a30f93d62a02539ac6921ce90afb29879132029
wuhandashuaibi <907296194@qq.com>
2024-10-10 11:09:05 +0800

    add export onnx model

--- tools/deploy.py
+++ tools/deploy.py
@@ -174,19 +174,6 @@ def main():
 
     print('finished')
 
-    from contrib.fastbev_det.models.necks.view_transform import VoxelProjection
-    input = torch.ones((4,336,80,128)).float().cuda()
-    model = VoxelProjection()
-    out = model.eval()(input)
-    
-    torch.onnx.export(model, input, "./work_dirs/deploy/voxel_projection.onnx", opset_version=11,
-            input_names = ['input'],
-            output_names = ['output'],
-            operator_export_type=torch.onnx.OperatorExportTypes.ONNX_ATEN_FALLBACK)
-    
-    
-    
-
 
 if __name__ == '__main__':
     main()
