b7951da0354c1c61dfc34531eb81094029aaf7b7
rlan <brianlanbo@gmail.com>
2024-10-16 13:14:38 +0800

    [wip] created PlanarPolyline3DLoss

--- prefusion/loss/planar.py
+++ prefusion/loss/planar.py
@@ -1,4 +1,4 @@
-import functools
+from functools import partial
 
 import torch
 import torch.nn as nn
@@ -6,21 +6,22 @@ import torch.nn as nn
 from prefusion.loss.basic import dual_focal_loss, SegIouLoss
 
 
+def complete_loss_name(loss_name_prefix, sub_item_name: str = ""):
+    if sub_item_name:
+        return f"{loss_name_prefix}_{sub_item_name}_loss"
+    return f"{loss_name_prefix}_loss"
+
+
 class PlanarBbox3DLoss(nn.Module):
     def __init__(self, *args, loss_name_prefix="plnrbox3d", class_weights=None, loss_weights=None, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.loss_name_prefix = loss_name_prefix
-        self.total_loss_name = self.complete_loss_name()
+        self.total_loss_name = complete_loss_name(self.loss_name_prefix)
         self.loss_weights = {"seg": 1.0, "cen": 1.0, "reg": 1.0}
         self.loss_weights.update(loss_weights or {})
         self.class_weights = class_weights
         self.reduction_dim = (0, 2, 3)
         self.iou_loss = SegIouLoss(pred_logits=True, reduction_dim=self.reduction_dim)
-
-    def complete_loss_name(self, sub_item_name: str = ""):
-        if sub_item_name:
-            return f"{self.loss_name_prefix}_{sub_item_name}_loss"
-        return f"{self.loss_name_prefix}_loss"
 
     @staticmethod
     def _ensure_shape_nchw(tensor_dict):
@@ -41,8 +42,8 @@ class PlanarBbox3DLoss(nn.Module):
             **self._cen_loss(pred["cen"], label["cen"], fg_mask=label["seg"][:, 0:1]),
             **self._reg_loss(pred["reg"], label["reg"], fg_mask=label["seg"][:, 0:1]),
         )
-        _L = self.complete_loss_name
-        loss[self.total_loss_name] = loss[_L("seg")] + loss[_L("cen")] + loss[_L("reg")]
+        L = partial(complete_loss_name, self.loss_name_prefix)
+        loss[self.total_loss_name] = loss[L("seg")] + loss[L("cen")] + loss[L("reg")]
         return loss
 
     def _seg_loss(self, pred, label, iou_loss_weight=1.0, dual_focal_loss_weight=1.0):
@@ -50,24 +51,24 @@ class PlanarBbox3DLoss(nn.Module):
         iou_loss = self.iou_loss(pred, label)
         _class_weights = torch.ones(pred.shape[1]) if self.class_weights is None else torch.tensor(self.class_weights)
         loss_dict = {}
-        _L = self.complete_loss_name
-        seg_iou_loss_by_channel = {_L(f"seg_iou_{i}"): l * _class_weights[i] for i, l in enumerate(iou_loss)}
-        seg_dual_focal_loss_by_channel = {_L(f"seg_dual_focal_{i}"): l * _class_weights[i] for i, l in enumerate(dual_loss)}
+        L = partial(complete_loss_name, self.loss_name_prefix)
+        seg_iou_loss_by_channel = {L(f"seg_iou_{i}"): l * _class_weights[i] for i, l in enumerate(iou_loss)}
+        seg_dual_focal_loss_by_channel = {L(f"seg_dual_focal_{i}"): l * _class_weights[i] for i, l in enumerate(dual_loss)}
         loss_dict.update(seg_iou_loss_by_channel)
         loss_dict.update(seg_dual_focal_loss_by_channel)
-        loss_dict[_L("seg_iou")]  = iou_loss_weight * sum(seg_iou_loss_by_channel.values()) / _class_weights.sum() # functools.reduce(lambda x, y: x + y, seg_iou_loss_by_channel.values())
-        loss_dict[_L("seg_dual_focal")] = dual_focal_loss_weight * sum(seg_dual_focal_loss_by_channel.values()) / _class_weights.sum()
-        loss_dict[_L("seg")] = (loss_dict[_L("seg_dual_focal")] + loss_dict[_L("seg_iou")]) * self.loss_weights["seg"]
+        loss_dict[L("seg_iou")]  = iou_loss_weight * sum(seg_iou_loss_by_channel.values()) / _class_weights.sum() # functools.reduce(lambda x, y: x + y, seg_iou_loss_by_channel.values())
+        loss_dict[L("seg_dual_focal")] = dual_focal_loss_weight * sum(seg_dual_focal_loss_by_channel.values()) / _class_weights.sum()
+        loss_dict[L("seg")] = (loss_dict[L("seg_dual_focal")] + loss_dict[L("seg_iou")]) * self.loss_weights["seg"]
         return loss_dict
 
     def _cen_loss(self, pred, label, fg_mask, fg_weight=1.0, bg_weight=1.0):
         dual_loss = dual_focal_loss(pred, label, reduction="none").mean()
         fg_dual_loss = (dual_focal_loss(pred, label, reduction="none") * fg_mask).sum() / fg_mask.sum()
         loss_dict = {}
-        _L = self.complete_loss_name
-        loss_dict[_L("cen_dual_focal")] = bg_weight * dual_loss
-        loss_dict[_L("cen_fg_dual_focal")] = fg_weight * fg_dual_loss
-        loss_dict[_L("cen")] = (loss_dict[_L("cen_dual_focal")] + loss_dict[_L("cen_fg_dual_focal")]) * self.loss_weights["cen"]
+        L = partial(complete_loss_name, self.loss_name_prefix)
+        loss_dict[L("cen_dual_focal")] = bg_weight * dual_loss
+        loss_dict[L("cen_fg_dual_focal")] = fg_weight * fg_dual_loss
+        loss_dict[L("cen")] = (loss_dict[L("cen_dual_focal")] + loss_dict[L("cen_fg_dual_focal")]) * self.loss_weights["cen"]
         return loss_dict
 
     def _reg_loss(
@@ -87,7 +88,7 @@ class PlanarBbox3DLoss(nn.Module):
     ):
         assert pred.shape[1] == 20
         loss_dict = {}
-        _L = self.complete_loss_name
+        L = partial(complete_loss_name, self.loss_name_prefix)
         mask_sum = fg_mask.sum()
         def _calc_sub_loss(_weight, channel_slice):
             l1 = nn.L1Loss(reduction="none")(pred[:, channel_slice, ...], label[:, channel_slice, ...])
@@ -95,15 +96,94 @@ class PlanarBbox3DLoss(nn.Module):
             return _weight * (fg_mask * l1).sum() / mask_sum / n_channels
 
         loss_dict = {
-            _L("reg_center_xy"): _calc_sub_loss(center_xy_weight, slice(0, 2)),
-            _L("reg_center_z"): _calc_sub_loss(center_z_weight, slice(2, 3)),
-            _L("reg_size"): _calc_sub_loss(size_weight, slice(3, 6)),
-            _L("reg_unit_xvec"): _calc_sub_loss(unit_xvec_weight, slice(6, 9)),
-            _L("reg_abs_xvec"): _calc_sub_loss(abs_xvec_weight, slice(9, 12)),
-            _L("reg_xvec_product"): _calc_sub_loss(xvec_product_weight, slice(12, 14)),
-            _L("reg_abs_roll_angle"): _calc_sub_loss(abs_roll_angle_weight, slice(14, 16)),
-            _L("reg_roll_angle_product"): _calc_sub_loss(roll_angle_product_weight, slice(16, 17)),
-            _L("reg_velo"): _calc_sub_loss(velo_weight, slice(17, 20)),
+            L("reg_center_xy"): _calc_sub_loss(center_xy_weight, slice(0, 2)),
+            L("reg_center_z"): _calc_sub_loss(center_z_weight, slice(2, 3)),
+            L("reg_size"): _calc_sub_loss(size_weight, slice(3, 6)),
+            L("reg_unit_xvec"): _calc_sub_loss(unit_xvec_weight, slice(6, 9)),
+            L("reg_abs_xvec"): _calc_sub_loss(abs_xvec_weight, slice(9, 12)),
+            L("reg_xvec_product"): _calc_sub_loss(xvec_product_weight, slice(12, 14)),
+            L("reg_abs_roll_angle"): _calc_sub_loss(abs_roll_angle_weight, slice(14, 16)),
+            L("reg_roll_angle_product"): _calc_sub_loss(roll_angle_product_weight, slice(16, 17)),
+            L("reg_velo"): _calc_sub_loss(velo_weight, slice(17, 20)),
         }
-        loss_dict[_L("reg")] = sum(loss_dict.values()) * self.loss_weights["reg"]
+        loss_dict[L("reg")] = sum(loss_dict.values()) * self.loss_weights["reg"]
+        return loss_dict
+
+
+class PlanarPolyline3DLoss(nn.Module):
+    def __init__(self, *args, loss_name_prefix="plnrplyln3d", class_weights=None, loss_weights=None, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.loss_name_prefix = loss_name_prefix
+        self.total_loss_name = complete_loss_name(self.loss_name_prefix)
+        self.loss_weights = {"seg": 1.0, "reg": 1.0}
+        self.loss_weights.update(loss_weights or {})
+        self.class_weights = class_weights
+        self.reduction_dim = (0, 2, 3)
+        self.iou_loss = SegIouLoss(pred_logits=True, reduction_dim=self.reduction_dim)
+
+    @staticmethod
+    def _ensure_shape_nchw(tensor_dict):
+        for k, v in tensor_dict.items():
+            if v.dim() == 2:
+                tensor_dict[k] = v.unsqueeze(0).unsqueeze(0)
+            elif v.dim() == 3:
+                tensor_dict[k] = v.unsqueeze(1)
+        return tensor_dict
+
+    def forward(self, pred, label):
+        assert set(pred.keys()) == set(label.keys()) == {"seg", "reg"}
+        pred = self._ensure_shape_nchw(pred)
+        label = self._ensure_shape_nchw(label)
+
+        loss = dict(
+            **self._seg_loss(pred["seg"], label["seg"]),
+            **self._reg_loss(pred["reg"], label["reg"], fg_mask=label["seg"][:, 0:1]),
+        )
+        L = partial(complete_loss_name, self.loss_name_prefix)
+        loss[self.total_loss_name] = loss[L("seg")] + loss[L("reg")]
+        return loss
+
+    def _seg_loss(self, pred, label, iou_loss_weight=1.0, dual_focal_loss_weight=1.0):
+        dual_loss = dual_focal_loss(pred, label, reduction="none").mean(dim=self.reduction_dim)
+        iou_loss = self.iou_loss(pred, label)
+        _class_weights = torch.ones(pred.shape[1]) if self.class_weights is None else torch.tensor(self.class_weights)
+        loss_dict = {}
+        L = partial(complete_loss_name, self.loss_name_prefix)
+        seg_iou_loss_by_channel = {L(f"seg_iou_{i}"): l * _class_weights[i] for i, l in enumerate(iou_loss)}
+        seg_dual_focal_loss_by_channel = {L(f"seg_dual_focal_{i}"): l * _class_weights[i] for i, l in enumerate(dual_loss)}
+        loss_dict.update(seg_iou_loss_by_channel)
+        loss_dict.update(seg_dual_focal_loss_by_channel)
+        loss_dict[L("seg_iou")]  = iou_loss_weight * sum(seg_iou_loss_by_channel.values()) / _class_weights.sum() # functools.reduce(lambda x, y: x + y, seg_iou_loss_by_channel.values())
+        loss_dict[L("seg_dual_focal")] = dual_focal_loss_weight * sum(seg_dual_focal_loss_by_channel.values()) / _class_weights.sum()
+        loss_dict[L("seg")] = (loss_dict[L("seg_dual_focal")] + loss_dict[L("seg_iou")]) * self.loss_weights["seg"]
+        return loss_dict
+
+    def _reg_loss(
+        self,
+        pred,
+        label,
+        fg_mask,
+        dist_weight=1.0,
+        vert_vec_weight=1.0,
+        abs_dir_weight=1.0,
+        dir_product_weight=1.0,
+        height_weight=1.0,
+    ):
+        assert pred.shape[1] == 7
+        loss_dict = {}
+        L = partial(complete_loss_name, self.loss_name_prefix)
+        mask_sum = fg_mask.sum()
+        def _calc_sub_loss(_weight, channel_slice):
+            l1 = nn.L1Loss(reduction="none")(pred[:, channel_slice, ...], label[:, channel_slice, ...])
+            n_channels = l1.shape[1]
+            return _weight * (fg_mask * l1).sum() / mask_sum / n_channels
+
+        loss_dict = {
+            L("reg_center_xy"): _calc_sub_loss(dist_weight, slice(0, 1)),
+            L("reg_center_z"): _calc_sub_loss(vert_vec_weight, slice(1, 3)),
+            L("reg_size"): _calc_sub_loss(abs_dir_weight, slice(3, 5)),
+            L("reg_unit_xvec"): _calc_sub_loss(dir_product_weight, slice(5, 6)),
+            L("reg_abs_xvec"): _calc_sub_loss(height_weight, slice(6, 7)),
+        }
+        loss_dict[L("reg")] = sum(loss_dict.values()) * self.loss_weights["reg"]
         return loss_dict
--- tests/prefusion/loss/test_loss.py
+++ tests/prefusion/loss/test_loss.py
@@ -5,12 +5,12 @@ import torch
 import numpy as np
 
 from prefusion.loss.basic import SegIouLoss, seg_iou
-from prefusion.loss.planar import PlanarBbox3DLoss
+from prefusion.loss.planar import PlanarBbox3DLoss, PlanarPolyline3DLoss
 
 _approx = functools.partial(pytest.approx, rel=1e-5)
 
 @pytest.fixture
-def seg_pred():
+def bx_seg_pred():
     return torch.tensor([[
         [[0.0, 0.1,  0.2, 0.3, 0.1],
          [0.0, 0.2,  0.8, 0.1, 0.1],
@@ -25,7 +25,7 @@ def seg_pred():
 
 
 @pytest.fixture
-def seg_label():
+def bx_seg_label():
     return torch.tensor([[
         [[0, 0, 0, 0, 0],
          [0, 0, 1, 0, 0],
@@ -40,7 +40,7 @@ def seg_label():
 
 
 @pytest.fixture
-def seg_mask():
+def bx_seg_mask():
     return torch.tensor([[
         [[1, 1, 1, 0, 0],
          [1, 1, 1, 0, 0],
@@ -55,7 +55,7 @@ def seg_mask():
 
 
 @pytest.fixture
-def cen_pred():
+def bx_cen_pred():
     return torch.tensor([[
         [[-79, -10, 0.0, -10, -5],
          [-80, 0.0,  10, 0.0, -5],
@@ -65,7 +65,7 @@ def cen_pred():
 
 
 @pytest.fixture
-def cen_label():
+def bx_cen_label():
     return torch.tensor([[
         [[0, 0.1, 0.5, 0.1, 0],
          [0, 0.5, 1.0, 0.5, 0],
@@ -75,7 +75,7 @@ def cen_label():
 
 
 @pytest.fixture
-def reg_pred():
+def bx_reg_pred():
     return torch.tensor([[
         [[0, 0.0, 0.0, 0.0, 0],
          [0, 0.0, 0.5, 0.0, 0],
@@ -90,7 +90,7 @@ def reg_pred():
 
 
 @pytest.fixture
-def reg_label():
+def bx_reg_label():
     return torch.tensor([[
         [[0, 0.0, 0.0, 0.0, 0],
          [0, 0.0, 0.8, 0.0, 0],
@@ -105,7 +105,7 @@ def reg_label():
 
 
 @pytest.fixture
-def reg_channel_weights():
+def bx_reg_channel_weights():
     return {
         "center_xy_weight": 1.0,
         "center_z_weight": 0.0,
@@ -119,41 +119,41 @@ def reg_channel_weights():
     }
 
 
-def test_seg_iou(seg_pred, seg_label):
-    assert seg_iou(seg_pred[0, 0], seg_label[0, 0]).item() == _approx(0.51724)
-    assert seg_iou(seg_pred[0, 1], seg_label[0, 1]).item() == _approx(0.541666)
-    assert seg_iou(seg_pred, seg_label, dim=(0, 2, 3)).tolist() == _approx([0.51724, 0.541666])
-    assert seg_iou(seg_pred[0, 1], torch.zeros_like(seg_pred[0, 1])).item() == _approx(0.75714)
+def test_seg_iou(bx_seg_pred, bx_seg_label):
+    assert seg_iou(bx_seg_pred[0, 0], bx_seg_label[0, 0]).item() == _approx(0.51724)
+    assert seg_iou(bx_seg_pred[0, 1], bx_seg_label[0, 1]).item() == _approx(0.541666)
+    assert seg_iou(bx_seg_pred, bx_seg_label, dim=(0, 2, 3)).tolist() == _approx([0.51724, 0.541666])
+    assert seg_iou(bx_seg_pred[0, 1], torch.zeros_like(bx_seg_pred[0, 1])).item() == _approx(0.75714)
 
 
-def test_seg_iou_loss_sigmoid(seg_pred, seg_label):
+def test_seg_iou_loss_sigmoid(bx_seg_pred, bx_seg_label):
     iou_loss = SegIouLoss(method="linear", pred_logits=True)
-    assert iou_loss(seg_pred, seg_label).item() == _approx(0.7773743)
+    assert iou_loss(bx_seg_pred, bx_seg_label).item() == _approx(0.7773743)
 
 
-def test_seg_iou_loss_no_sigmoid(seg_pred, seg_label):
+def test_seg_iou_loss_no_sigmoid(bx_seg_pred, bx_seg_label):
     iou_loss = SegIouLoss(method="linear", pred_logits=False)
-    assert iou_loss(seg_pred, seg_label).item() == _approx(0.5083333)
+    assert iou_loss(bx_seg_pred, bx_seg_label).item() == _approx(0.5083333)
     
 
-def test_seg_iou_loss(seg_pred, seg_label):
+def test_seg_iou_loss(bx_seg_pred, bx_seg_label):
     iou_loss = SegIouLoss(method="linear", pred_logits=False, reduction_dim=(0, 2, 3))
-    assert iou_loss(seg_pred, seg_label).tolist() == _approx([0.482756, 0.458334])
+    assert iou_loss(bx_seg_pred, bx_seg_label).tolist() == _approx([0.482756, 0.458334])
 
 
-def test_seg_iou_loss_sigmoid_log(seg_pred, seg_label):
+def test_seg_iou_loss_sigmoid_log(bx_seg_pred, bx_seg_label):
     iou_loss = SegIouLoss(method="log", pred_logits=True, reduction_dim=(0, 2, 3))
-    assert iou_loss(seg_pred, seg_label).tolist() == _approx([1.468862, 1.298853])
+    assert iou_loss(bx_seg_pred, bx_seg_label).tolist() == _approx([1.468862, 1.298853])
 
 
-def test_seg_iou_loss_with_mask(seg_pred, seg_label, seg_mask):
+def test_seg_iou_loss_with_mask(bx_seg_pred, bx_seg_label, bx_seg_mask):
     iou_loss = SegIouLoss(method="linear", pred_logits=False, reduction_dim=(0, 2, 3))
-    assert iou_loss(seg_pred, seg_label, mask=seg_mask).tolist() == _approx([0.425, 0.5106383])
+    assert iou_loss(bx_seg_pred, bx_seg_label, mask=bx_seg_mask).tolist() == _approx([0.425, 0.5106383])
 
 
-def test_planar_bbox3d_seg_loss(seg_pred, seg_label):
+def test_planar_bbox3d_seg_loss(bx_seg_pred, bx_seg_label):
     planar_bbox3d_loss = PlanarBbox3DLoss(loss_name_prefix="plnrbox3d", class_weights=[0.3, 0.7])
-    seg_loss = planar_bbox3d_loss._seg_loss(seg_pred, seg_label)
+    seg_loss = planar_bbox3d_loss._seg_loss(bx_seg_pred, bx_seg_label)
     assert set(seg_loss.keys()) == {
         "plnrbox3d_seg_iou_0_loss", "plnrbox3d_seg_iou_1_loss", "plnrbox3d_seg_iou_loss",
         "plnrbox3d_seg_dual_focal_0_loss", "plnrbox3d_seg_dual_focal_1_loss", "plnrbox3d_seg_dual_focal_loss",
@@ -168,20 +168,20 @@ def test_planar_bbox3d_seg_loss(seg_pred, seg_label):
     assert seg_loss["plnrbox3d_seg_loss"] == _approx(2.5408336)
 
 
-def test_planar_bbox3d_cen_loss(cen_pred, cen_label):
+def test_planar_bbox3d_cen_loss(bx_cen_pred, bx_cen_label):
     planar_bbox3d_loss = PlanarBbox3DLoss(loss_name_prefix="plnrbox3d")
     cen_mask = torch.zeros((1, 1, 4, 5))
     cen_mask[:, :, :, 1:4] = 1
-    cen_loss = planar_bbox3d_loss._cen_loss(cen_pred, cen_label, cen_mask)
+    cen_loss = planar_bbox3d_loss._cen_loss(bx_cen_pred, bx_cen_label, cen_mask)
     assert set(cen_loss.keys()) == {"plnrbox3d_cen_dual_focal_loss", "plnrbox3d_cen_fg_dual_focal_loss", "plnrbox3d_cen_loss"}
     assert cen_loss["plnrbox3d_cen_dual_focal_loss"] == _approx(0.430634)
     assert cen_loss["plnrbox3d_cen_fg_dual_focal_loss"] == _approx(0.7132555)
     assert cen_loss["plnrbox3d_cen_loss"] == _approx(1.1438895)
 
-def test_planar_bbox3d_reg_loss(reg_pred, reg_label, reg_channel_weights):
+def test_planar_bbox3d_reg_loss(bx_reg_pred, bx_reg_label, bx_reg_channel_weights):
     planar_bbox3d_loss = PlanarBbox3DLoss(loss_name_prefix="plnrbox3d")
     reg_mask = torch.ones((1, 1, 4, 5))
-    reg_loss = planar_bbox3d_loss._reg_loss(reg_pred, reg_label, reg_mask, **reg_channel_weights)
+    reg_loss = planar_bbox3d_loss._reg_loss(bx_reg_pred, bx_reg_label, reg_mask, **bx_reg_channel_weights)
     assert set(reg_loss.keys()) == {
         "plnrbox3d_reg_center_xy_loss",
         "plnrbox3d_reg_center_z_loss",
@@ -199,20 +199,20 @@ def test_planar_bbox3d_reg_loss(reg_pred, reg_label, reg_channel_weights):
     assert reg_loss["plnrbox3d_reg_loss"] == _approx(0.1275)
 
 
-def test_planar_bbox3d_reg_loss_with_mask(reg_pred, reg_label, reg_channel_weights):
+def test_planar_bbox3d_reg_loss_with_mask(bx_reg_pred, bx_reg_label, bx_reg_channel_weights):
     planar_bbox3d_loss = PlanarBbox3DLoss(loss_name_prefix="plnrbox3d")
     reg_mask = torch.zeros((1, 1, 4, 5))
     reg_mask[:, :, 2:, :] = 1
-    reg_loss = planar_bbox3d_loss._reg_loss(reg_pred, reg_label, reg_mask, **reg_channel_weights)
+    reg_loss = planar_bbox3d_loss._reg_loss(bx_reg_pred, bx_reg_label, reg_mask, **bx_reg_channel_weights)
     assert reg_loss["plnrbox3d_reg_center_xy_loss"] == _approx(0.04)
     assert reg_loss["plnrbox3d_reg_xvec_product_loss"] == _approx(0.08)
     assert reg_loss["plnrbox3d_reg_loss"] == _approx(0.12)
 
 
-def test_planar_bbox3d_loss(seg_pred, cen_pred, reg_pred, seg_label, cen_label, reg_label):
+def test_planar_bbox3d_loss(bx_seg_pred, bx_cen_pred, bx_reg_pred, bx_seg_label, bx_cen_label, bx_reg_label):
     planar_bbox3d_loss = PlanarBbox3DLoss(loss_name_prefix="plnrbox3d", class_weights=[0.3, 0.7])
-    pred = {"seg": seg_pred, "cen": cen_pred, "reg": reg_pred}
-    label = {"seg": seg_label, "cen": cen_label, "reg": reg_label}
+    pred = {"seg": bx_seg_pred, "cen": bx_cen_pred, "reg": bx_reg_pred}
+    label = {"seg": bx_seg_label, "cen": bx_cen_label, "reg": bx_reg_label}
     loss = planar_bbox3d_loss(pred, label)
     assert set(loss.keys()) == {
         "plnrbox3d_seg_iou_0_loss", "plnrbox3d_seg_iou_1_loss", "plnrbox3d_seg_iou_loss",
@@ -247,3 +247,55 @@ def test_planar_bbox3d_loss(seg_pred, cen_pred, reg_pred, seg_label, cen_label, 
     assert loss["plnrbox3d_reg_loss"] == _approx(1.55)
     
     assert loss["plnrbox3d_loss"] == _approx(4.7525781)
+
+
+@pytest.fixture
+def plyl_reg_pred():
+    pass
+
+
+@pytest.fixture
+def plyl_reg_label():
+    pass
+
+
+@pytest.fixture
+def plyl_reg_channel_weights():
+    return {
+        "dist_weight": 1.0,
+        "vert_vec_weight": 0.0,
+        "abs_dir_weight": 0.0,
+        "dir_product_weight": 0.0,
+        "height_weight": 0.0,
+    }
+
+
+def test_planar_polyline3d_reg_loss(plyl_reg_pred, plyl_reg_label, plyl_reg_channel_weights):
+    planar_polyline3d_loss = PlanarPolyline3DLoss(loss_name_prefix="plnrplyln3d")
+    reg_mask = torch.ones((1, 1, 4, 5))
+    reg_loss = planar_polyline3d_loss._reg_loss(plyl_reg_pred, plyl_reg_label, reg_mask, **plyl_reg_channel_weights)
+    assert set(reg_loss.keys()) == {
+        "plnrplyln3d_reg_center_xy_loss",
+        "plnrplyln3d_reg_center_z_loss",
+        "plnrplyln3d_reg_size_loss",
+        "plnrplyln3d_reg_unit_xvec_loss",
+        "plnrplyln3d_reg_abs_xvec_loss",
+        "plnrplyln3d_reg_xvec_product_loss",
+        "plnrplyln3d_reg_abs_roll_angle_loss",
+        "plnrplyln3d_reg_roll_angle_product_loss",
+        "plnrplyln3d_reg_velo_loss",
+        "plnrplyln3d_reg_loss",
+    }
+    assert reg_loss["plnrplyln3d_reg_center_xy_loss"] == _approx(0.0425)
+    assert reg_loss["plnrplyln3d_reg_xvec_product_loss"] == _approx(0.085)
+    assert reg_loss["plnrplyln3d_reg_loss"] == _approx(0.1275)
+
+
+def test_planar_polyline3d_reg_loss_with_mask(plyl_reg_pred, plyl_reg_label, plyl_reg_channel_weights):
+    planar_bbox3d_loss = PlanarPolyline3DLoss(loss_name_prefix="plnrplyln3d")
+    reg_mask = torch.zeros((1, 1, 4, 5))
+    reg_mask[:, :, 2:, :] = 1
+    reg_loss = planar_bbox3d_loss._reg_loss(plyl_reg_pred, plyl_reg_label, reg_mask, **plyl_reg_channel_weights)
+    assert reg_loss["plnrplyln3d_reg_center_xy_loss"] == _approx(0.04)
+    assert reg_loss["plnrplyln3d_reg_xvec_product_loss"] == _approx(0.08)
+    assert reg_loss["plnrplyln3d_reg_loss"] == _approx(0.12)
