f3b808353c70ae01a0b771af43d7ec27a83165c3
wuhandashuaibi <907296194@qq.com>
2024-10-24 09:42:44 +0800

    pretrain data process

--- tools/dataset_converters/convert_avp_data.py
+++ tools/dataset_converters/convert_avp_data.py
@@ -1,12 +1,10 @@
 import mmengine
 from pathlib import Path as P
-
+import os
 
 with open("data/avp/train.txt", 'r') as f:
     data = f.readlines()
 
-with open("data/avp/val.txt", 'r') as f:
-    data += f.readlines()
 
 dump_data = dict(
     avp=dict(
@@ -16,10 +14,12 @@ dump_data = dict(
 for idx, line in enumerate(data):
     line = line.strip()
     frame_id = str(idx).zfill(8)
-    dump_data['avp']['frame_info'][frame_id] = dict(
-        camera_image={"VCAMERA_FISHEYE_FRONT": P('AVP_raw_dataset') / P(line+".jpg")},
-        camera_image_seg={"VCAMERA_FISHEYE_FRONT": P('AVP_raw_dataset') / P('mask') / P(line+".png")},
-        camera_image_depth={"VCAMERA_FISHEYE_FRONT": None}
-    )
+    if os.path.exists(P("data/avp") / P('AVP_raw_dataset') / P(line+".jpg")) and os.path.exists(P("data/avp") / P('AVP_raw_dataset') / P('mask') / P(line+".png")):
+        dump_data['avp']['frame_info'][frame_id] = dict(
+            camera_image={"VCAMERA_FISHEYE_FRONT": P('AVP_raw_dataset') / P(line+".jpg")},
+            camera_image_seg={"VCAMERA_FISHEYE_FRONT": P('AVP_raw_dataset') / P('mask') / P(line+".png")},
+            camera_image_depth={"VCAMERA_FISHEYE_FRONT": None}
+        )
+
 
 mmengine.dump(dump_data, "data/avp/avp_train.pkl")
