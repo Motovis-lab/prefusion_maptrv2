8a0d74efc39234f5c1670245dcdc7dec34ced545
AlphaLFC <398505136@qq.com>
2024-11-04 10:01:03 +0800

    debugging configs

--- contrib/fastray_planar/configs/fastray_planar_stream_model.py
+++ contrib/fastray_planar/configs/fastray_planar_stream_model.py
@@ -118,12 +118,12 @@ dictionary_polygons = dict(
 ## camera configs for model inputs
 
 camera_groups = dict(
-    pv_front=['VCAMERA_PERSPECTIVE_FRONT'],
-    pv_sides=['VCAMERA_PERSPECTIVE_FRONT_LEFT',
-              'VCAMERA_PERSPECTIVE_FRONT_RIGHT',
-              'VCAMERA_PERSPECTIVE_BACK_LEFT',
-              'VCAMERA_PERSPECTIVE_BACK_RIGHT',
-              'VCAMERA_PERSPECTIVE_BACK'],
+    perspectives=['VCAMERA_PERSPECTIVE_FRONT',
+                  'VCAMERA_PERSPECTIVE_FRONT_LEFT',
+                  'VCAMERA_PERSPECTIVE_FRONT_RIGHT',
+                  'VCAMERA_PERSPECTIVE_BACK_LEFT',
+                  'VCAMERA_PERSPECTIVE_BACK_RIGHT',
+                  'VCAMERA_PERSPECTIVE_BACK'],
     fisheyes=['VCAMERA_FISHEYE_FRONT',
               'VCAMERA_FISHEYE_LEFT',
               'VCAMERA_FISHEYE_BACK',
@@ -144,6 +144,29 @@ camera_resolution_configs=dict(
     VCAMERA_FISHEYE_LEFT=resolution_fisheyes,
     VCAMERA_FISHEYE_BACK=resolution_fisheyes,
     VCAMERA_FISHEYE_RIGHT=resolution_fisheyes)
+
+
+debug_mode = True
+
+if debug_mode:
+    batch_size = 1
+    num_workers = 0
+    transforms = [
+        dict(type='RenderIntrinsic', resolutions=camera_resolution_configs)
+    ]
+else:
+    batch_size = 4
+    num_workers = 4
+    transforms = [
+        dict(type='RandomRenderExtrinsic'),
+        dict(type='RenderIntrinsic', resolutions=camera_resolution_configs),
+        dict(type='RandomRotateSpace'),
+        dict(type='RandomMirrorSpace'),
+        dict(type='RandomImageISP', prob=0.2),
+        dict(type='RandomSetIntrinsicParam', prob=0.2, jitter_ratio=0.01),
+        dict(type='RandomSetExtrinsicParam', prob=0.2, angle=1, translation=0.02)
+    ]
+
 
 ## GroupBatchDataset configs
 train_dataset = dict(
@@ -172,24 +195,16 @@ train_dataset = dict(
             dictionary=dict(classes=['class.parking.parking_slot']),
             tensor_smith=dict(type='PlanarParkingSlot3D', voxel_shape=voxel_shape, voxel_range=voxel_range))
     ),
-    transforms=[
-        # dict(type='RandomRenderExtrinsic'),
-        dict(type='RenderIntrinsic', resolutions=camera_resolution_configs),
-        # dict(type='RandomRotateSpace'),
-        # dict(type='RandomMirrorSpace'),
-        # dict(type='RandomImageISP'),
-        # dict(type='RandomSetIntrinsicParam', prob=0.2, jitter_ratio=0.01),
-        # dict(type='RandomSetExtrinsicParam', prob=0.2, angle=1, translation=0.02)
-    ],
+    transforms=transforms,
     phase="train",
-    batch_size=4,
-    possible_group_sizes=10,
+    batch_size=batch_size,
+    possible_group_sizes=60,
     possible_frame_intervals=5,
 )
 
 ## dataloader configs
 train_dataloader = dict(
-    num_workers=4,
+    num_workers=num_workers,
     collate_fn=dict(type="collate_dict"),
     dataset=train_dataset
 )
@@ -202,8 +217,7 @@ bev_mode = False
 # backbones
 camera_feat_channels = 128
 backbones = dict(
-    pv_front=dict(type='VoVNetFPN', out_stride=8, out_channels=camera_feat_channels),
-    pv_sides=dict(type='VoVNetFPN', out_stride=8, out_channels=camera_feat_channels),
+    perspectives=dict(type='VoVNetFPN', out_stride=8, out_channels=camera_feat_channels),
     fisheyes=dict(type='VoVNetFPN', out_stride=8, out_channels=camera_feat_channels))
 # spatial_transform
 spatial_transform = dict(
@@ -222,6 +236,7 @@ temporal_transform = dict(
 voxel_fusion = dict(
     type='VoxelStreamFusion',
     in_channels=camera_feat_channels,
+    mid_channels=128,
     bev_mode=bev_mode)
 # heads
 heads = dict(
@@ -231,17 +246,17 @@ heads = dict(
                        out_channels=256,
                        repeat=3),
     bbox_3d=dict(type='PlanarHead',
-                 in_channels=128,
+                 in_channels=256,
                  mid_channels=128,
                  cen_seg_channels=3,
                  reg_channels=20),
     polyline_3d=dict(type='PlanarHead',
-                     in_channels=128,
+                     in_channels=256,
                      mid_channels=128,
                      cen_seg_channels=2,
                      reg_channels=7),
     parkingslot_3d=dict(type='PlanarHead',
-                        in_channels=128,
+                        in_channels=256,
                         mid_channels=128,
                         cen_seg_channels=5,
                         reg_channels=15)
@@ -252,8 +267,23 @@ bbox_3d_weight_scheme = dict(
              fg_weight=1.0,
              bg_weight=1),
     seg=dict(loss_weight=1.0,
+             iou_loss_weight=1,
+             dual_focal_loss_weight=2),
+    reg=dict(loss_weight=1.0))
+
+polyline_3d_weight_scheme = dict(
+    seg=dict(loss_weight=1.0,
              iou_loss_weight=2,
-             dual_focal_loss_weight=1),
+             dual_focal_loss_weight=5),
+    reg=dict(loss_weight=1.0))
+
+parkingslot_3d_weight_scheme = dict(
+    cen=dict(loss_weight=0.5,
+             fg_weight=1.0,
+             bg_weight=1),
+    seg=dict(loss_weight=1.0,
+             iou_loss_weight=1,
+             dual_focal_loss_weight=2),
     reg=dict(loss_weight=1.0))
 
 loss_cfg = dict(
@@ -263,10 +293,12 @@ loss_cfg = dict(
         weight_scheme=bbox_3d_weight_scheme),
     polyline_3d=dict(
         type='PlanarLoss',
-        loss_name_prefix='polyline_3d'),
+        loss_name_prefix='polyline_3d',
+        weight_scheme=polyline_3d_weight_scheme),
     parkingslot_3d=dict(
         type='PlanarLoss',
-        loss_name_prefix='parkingslot_3d')
+        loss_name_prefix='parkingslot_3d',
+        weight_scheme=parkingslot_3d_weight_scheme)
 )
 
 # metric configs
@@ -281,6 +313,7 @@ model = dict(
     voxel_fusion=voxel_fusion,
     heads=heads,
     loss_cfg=loss_cfg,
+    debug_mode=debug_mode,
 )
 
 ## log_processor
@@ -312,4 +345,7 @@ env_cfg = dict(
     dist_cfg=dict(backend='nccl'),
 )
 
+# work_dir = "./work_dirs/fastray_planar_stream_model_1103"
+work_dir = "./work_dirs/fastray_planar_stream_model_1103_infer"
+load_from = "./work_dirs/fastray_planar_stream_model_1103/epoch_50.pth"
 # resume = True
