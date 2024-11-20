6b16ee607437eab6248f7d2aec89dd0c9565d15a
AlphaLFC <398505136@qq.com>
2024-11-20 16:51:34 +0800

    minor changes

--- tools/debug_memory_usage.py
+++ tools/debug_memory_usage.py
@@ -1,6 +1,7 @@
 
 import time
 
+from tqdm import tqdm
 from mmengine.config import Config
 from prefusion.registry import RUNNERS
 from mmengine.runner import Runner 
@@ -9,12 +10,14 @@ from mmengine.runner import Runner
 
 
 def get_data(dataloader):
-
-    for group_idx, group_batch in enumerate(dataloader):
+    
+    total_size = len(dataloader)
+    for group_idx, group_batch in tqdm(enumerate(dataloader), total=total_size):
         for frame_idx, frame_batch in enumerate(group_batch):
             idx = group_idx * 1 + frame_idx
             time.sleep(0.01)
-            print(idx)
+            # print(f'{idx}/{total_size}')
+    print('DONE!')
             
 
 
@@ -23,8 +26,8 @@ if __name__ == '__main__':
     cfg = Config.fromfile('contrib/fastray_planar/configs/debug.py')
     runner = Runner.from_cfg(cfg)
     runner.logger.name = "prefusion"
-
-    dataloader = Runner.build_dataloader(runner._train_dataloader)
     
+    print('Please start htop, and observe the RAM usage!')
+    dataloader = Runner.build_dataloader(runner._train_dataloader)
     get_data(dataloader)
     
