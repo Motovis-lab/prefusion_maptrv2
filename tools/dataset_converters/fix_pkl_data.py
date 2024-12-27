7aafa2de64d31429080ea1cd8be90e6f206f7f5f
wuhan <pro.wuhan@gmail.com>
2024-12-27 16:49:32 +0800

    gene mv4d info v2

--- tools/dataset_converters/fix_pkl_data.py
+++ tools/dataset_converters/fix_pkl_data.py
@@ -3,7 +3,7 @@ from pathlib import Path as P
 
 file_names = P("/mnt/ssd1/wuhan/prefusion/data/MV4D_12V3L/").glob("mv_4d_infos_2023*.pkl")
 
-fish_filenames = ["VCAMERA_FISHEYE_FRONT", "VCAMERA_FISHEYE_LEFT", "VCAMERA_FISHEYE_BACK", "VCAMERA_FISHEYE_RIGHT"]
+fish_filenames = ["VCAMERA_FISHEYE_FRONT", "VCAMERA_FISHEYE_LEFT", "VCAMERA_FISHEYE_BACK", "VCAMERA_FISHEYE_RIGHT", "camera1", "camera5", "camera8", "camera11"]
 
 for file in file_names:
     data = mmengine.load(file)
@@ -11,5 +11,9 @@ for file in file_names:
     for key in data[scene_name]['scene_info']['camera_mask']:
         if key in fish_filenames:
             data[scene_name]['scene_info']['camera_mask'][key] = f"ego_mask/{key}.png"
+        if key not in fish_filenames:
+            data[scene_name]['scene_info']['calibration'][key]['camera_type'] = 'PerspectiveCamera'
+        elif key in fish_filenames:
+            data[scene_name]['scene_info']['calibration'][key]['camera_type'] = 'FisheyeCamera'
 
     mmengine.dump(data, file)
