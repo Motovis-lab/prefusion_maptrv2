44b05f87d1d188d2e937a14563858c6bc5053c1d
AlphaLFC <398505136@qq.com>
2025-02-14 23:24:49 +0800

    add demo_video.py

---
+++ tools/demo_video.py
@@ -0,0 +1,257 @@
+import sys
+sys.path.insert(0, "../../")
+
+import json
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+import virtual_camera as vc
+
+from pathlib import Path
+from tqdm import tqdm
+from moviepy.video.io.ffmpeg_writer import FFMPEG_VideoWriter
+
+NAME2COLOR = {
+    'passenger_car': (64, 64, 255),
+    'bus': (64, 64, 255),
+    'truck': (64, 64, 255),
+    'fire_engine': (128, 64, 64),
+    'motorcycle': (64, 64, 128),
+    'bicycle': (64, 64, 128),
+    'tricycle': (64, 64, 128),
+    'cleaning_cart': (64, 64, 128),
+    'shopping_cart': (64, 64, 128),
+    'stroller': (64, 64, 128),
+    'scooter': (64, 64, 128),
+    'arrow': (255, 255, 255),
+    'wheel_stopper': (64, 255, 64),
+    'speed_bump': (128, 128, 64),
+    'water_filled_barrier': (64, 64, 64),
+    'cement_pier': (64, 64, 64),
+    'fire_box': (128, 64, 64),
+    'distribution_box': (128, 64, 64),
+    'pillar_rectangle': (255, 128, 255),
+    'parking_lock': (128, 128, 128),
+    'waste_bin': (64, 64, 64),
+    'pillar_cylinder': (255, 128, 255),
+    'cone': (255, 0, 0),
+    'bollard': (64, 64, 64),
+    'roadblock': (64, 64, 64),
+    'stone_ball': (64, 64, 64),
+    'crash_barrel': (64, 64, 64),
+    'fire_hydrant': (128, 64, 64),
+    'warning_triangle': (255, 0, 0),
+    'charging_infra': (64, 64, 64),
+    'pedestrian': (255, 64, 64),
+}
+
+heading_objs = [
+    'pedestrian',
+    'passenger_car',
+    'bus',
+    'truck',
+    'fire_engine',
+    'motorcycle',
+    'bicycle',
+    'tricycle',
+    'cleaning_cart',
+    'shopping_cart',
+    'stroller',
+    'scooter',
+    'arrow'
+]
+
+cameras = {
+    'VCAMERA_FISHEYE_FRONT': vc.create_virtual_fisheye_camera((640, 384), (-120, 0, -90), [3.81686209, 0.0055232 , 0.74317797]),
+    'VCAMERA_FISHEYE_LEFT': vc.create_virtual_fisheye_camera((640, 384), (-135, 0, 0), [2.09698729, 1.06778281, 1.01063169]),
+    'VCAMERA_FISHEYE_BACK': vc.create_virtual_fisheye_camera((640, 384), (-120, 0, 90), [-1.05845655, -1.53496593e-04,  1.00320036]),
+    'VCAMERA_FISHEYE_RIGHT': vc.create_virtual_fisheye_camera((640, 384), (-135, 0, 180), [ 2.13621105, -1.0802392 ,  1.02806573]),
+}
+
+def get_box_bev(element):
+    class_name = element['class']
+    rotation = np.float32(element['rotation']).reshape(3, 3)
+    translation = element['translation']
+    size = element['size']
+    xvec = 0.5 * rotation[:, 0] * size[0]
+    yvec = 0.5 * rotation[:, 1] * size[1]
+    corner_points = np.array([
+        translation + xvec - yvec,
+        translation + xvec + yvec,
+        translation - xvec + yvec,
+        translation - xvec - yvec
+    ], dtype=np.float32)
+    heading_points = np.array([
+        corner_points[:2].mean(0),
+        translation
+    ], dtype=np.float32)
+    return class_name, corner_points, heading_points
+    
+
+def get_box_camera(element, camera):
+    class_name = element['class']
+    rotation = np.float32(element['rotation']).reshape(3, 3)
+    translation = element['translation']
+    size = element['size']
+    xvec = 0.5 * rotation[:, 0] * size[0]
+    yvec = 0.5 * rotation[:, 1] * size[1]
+    zvec = 0.5 * rotation[:, 2] * size[2]
+    line_segments = np.float32([
+        translation + xvec - yvec - zvec + yvec * np.arange(0, 2.1, 0.1).reshape(-1, 1),  # 01
+        translation + xvec + yvec - zvec - xvec * np.arange(0, 2.1, 0.1).reshape(-1, 1),  # 12
+        translation - xvec + yvec - zvec - yvec * np.arange(0, 2.1, 0.1).reshape(-1, 1),  # 23
+        translation - xvec - yvec - zvec + xvec * np.arange(0, 2.1, 0.1).reshape(-1, 1),  # 30
+        translation + xvec - yvec - zvec + zvec * np.arange(0, 2.1, 0.1).reshape(-1, 1),  # 04
+        translation + xvec + yvec - zvec + zvec * np.arange(0, 2.1, 0.1).reshape(-1, 1),  # 15
+        translation - xvec + yvec - zvec + zvec * np.arange(0, 2.1, 0.1).reshape(-1, 1),  # 26
+        translation - xvec - yvec - zvec + zvec * np.arange(0, 2.1, 0.1).reshape(-1, 1),  # 37
+        translation + xvec - yvec + zvec + yvec * np.arange(0, 2.1, 0.1).reshape(-1, 1),  # 45
+        translation + xvec + yvec + zvec - xvec * np.arange(0, 2.1, 0.1).reshape(-1, 1),  # 56
+        translation - xvec + yvec + zvec - yvec * np.arange(0, 2.1, 0.1).reshape(-1, 1),  # 67
+        translation - xvec - yvec + zvec + xvec * np.arange(0, 2.1, 0.1).reshape(-1, 1),  # 74
+    ])
+
+    line_segments_camera = []
+    for line in line_segments:
+        camera_points = camera.extrinsic[0].T @ (line.T - np.float32(camera.extrinsic[1])[None].T)
+        uu, vv = camera.project_points_from_camera_to_image(camera_points)
+        valid = (uu >= 0) & (uu < 640) & (vv >= 0) & (vv < 320)
+        line_segments_camera.append(np.float32([uu[valid], vv[valid]]).T)
+
+    front_face = np.concatenate([
+        line_segments_camera[0],
+        line_segments_camera[5],
+        line_segments_camera[8][::-1],
+        line_segments_camera[4][::-1]
+    ])
+    return class_name, line_segments_camera, front_face
+
+
+def get_polyline_camera(polyline, camera):
+    line_segments = []
+    for i in range(len(polyline)-1):
+        start = polyline[i]
+        end = polyline[i+1]
+        segment = np.array([start, end])
+        segment_length = np.linalg.norm(end - start)
+        if segment_length > 0.5:
+            direction = (end - start) / segment_length
+            num_samples = int(np.ceil(segment_length / 0.5))
+            t = np.linspace(0, segment_length, num_samples+1)
+            interpolated = start + t[:, None] * direction
+            line_segments.append(interpolated)
+        else:
+            line_segments.append(segment)
+    line_segments_camera = []
+    for line_segment in line_segments:
+        camera_points = camera.extrinsic[0].T @ (line_segment.T - np.float32(camera.extrinsic[1])[None].T)
+        uu, vv = camera.project_points_from_camera_to_image(camera_points)
+        valid = (uu >= 0) & (uu < 640) & (vv >= 0) & (vv < 320)
+        if valid.sum() > 0:
+            line_segments_camera.append(np.float32([uu[valid], vv[valid]]).T)
+
+    return line_segments_camera
+
+
+def get_cam_img(ind, info, cameras):
+    imgs = []
+    for cam_id in cameras:
+        camera = cameras[cam_id]
+        cam_path = Path(cam_id) / (ind + '.jpg')
+        img = plt.imread(cam_path)[:320]
+        plt.figure(figsize=(8, 4), dpi=80)
+        fig = plt.gcf()
+        plt.imshow(img)
+        plt.axis('off')
+        plt.axis([0, 639, 319, 0])
+        
+        for slot in info['slots']:
+            slot = np.float32(slot).reshape(-1, 3)[[0, 1, 2, 3, 0]]
+            slot_line_segments_camera = get_polyline_camera(slot, camera)
+            for i, line in enumerate(slot_line_segments_camera):
+                if i == 0:
+                    continue
+                plt.plot(line[:, 0], line[:, 1], color=(0.6, 0.8, 0.6), linewidth=1)
+            if len(slot_line_segments_camera) > 0:
+                slot_area = np.concatenate(slot_line_segments_camera)
+                plt.fill(slot_area[:, 0], slot_area[:, 1], color=(0.6, 0.8, 0.6), alpha=0.2)
+
+        for element in info['bboxes']:
+            class_name, line_segments_camera, front_face = get_box_camera(element, camera)
+            color = np.float32(NAME2COLOR[class_name]) / 255
+            for line in line_segments_camera:
+                plt.plot(line[:, 0], line[:, 1], color=color)
+            if class_name in heading_objs:
+                plt.fill(front_face[:, 0], front_face[:, 1], color=color, alpha=0.7)
+
+        for polyline in info['polylines']:
+            polyline = np.float32(polyline).reshape(-1, 3)
+            line_segments_camera = get_polyline_camera(polyline, camera)
+            for line in line_segments_camera:
+                plt.plot(line[:, 0], line[:, 1], 'orange')
+
+        plt.tight_layout(pad=0)
+        fig.canvas.draw()
+        imgs.append(np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8).reshape(fig.canvas.get_width_height()[::-1] + (3,)))
+        plt.close()
+    img_cat = cv2.resize(np.concatenate(imgs, axis=0), (320, 640))
+    return img_cat
+    
+
+
+def get_bev_img(info):
+    voxel_range = [(-1, 3), (12, -12), (9, -9)]
+    plt.figure(figsize=(6, 8), dpi=80)
+    fig = plt.gcf()
+    fig.set_facecolor((0.8, 0.8, 0.8))
+    # fig.set_aspect('equal')
+    plt.xlim(voxel_range[2])
+    plt.ylim(voxel_range[1][::-1])
+    plt.axis('off')
+
+    for slot in info['slots']:
+        slot = np.float32(slot).reshape(-1, 3)
+        plt.fill(slot[[0, 1, 2, 3, 0], 1], slot[[0, 1, 2, 3, 0], 0], 'gray', alpha=0.5)
+        plt.plot(slot[[1, 2, 3, 0], 1], slot[[1, 2, 3, 0], 0], 'gray', linewidth=4)
+
+    for polyline in info['polylines']:
+        polyline = np.float32(polyline).reshape(-1, 3)
+        plt.plot(polyline[:, 1], polyline[:, 0], 'orange', linewidth=4)
+
+    for element in info['bboxes']:
+        class_name, corner_points, heading_points = get_box_bev(element)
+        color = np.float32(NAME2COLOR[class_name]) / 255
+        plt.fill(
+            corner_points[[0, 1, 2, 3, 0], 1], 
+            corner_points[[0, 1, 2, 3, 0], 0], 
+            color=color, alpha=0.5
+        )
+        plt.plot(
+            corner_points[[0, 1, 2, 3, 0], 1], 
+            corner_points[[0, 1, 2, 3, 0], 0], 
+            color=color
+        )
+        if class_name in heading_objs:
+            plt.plot(heading_points[:, 1], heading_points[:, 0], color=color)
+    plt.tight_layout(pad=0)
+    fig.canvas.draw()
+    bev_img = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8).reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    plt.close()
+    return bev_img
+
+
+if __name__ == '__main__':
+    results_root_path = Path('./gts_and_preds_new/')
+    all_inds = sorted([str(p.relative_to(results_root_path))[:-5] for p in results_root_path.glob('**/*.json')])
+
+    demo_video = FFMPEG_VideoWriter('prefusion_tda4_apa.mp4', size=(1280, 640), fps=10)
+    for ind in tqdm(all_inds):
+        json_path = results_root_path / (ind + '.json')
+        info_all = json.load(open(json_path))
+        img_cat = get_cam_img(ind, info_all['pred'], cameras)
+        bev_img_gt = get_bev_img(info_all['gt'])
+        bev_img_pred = get_bev_img(info_all['pred'])
+        img_final = np.concatenate([bev_img_pred, img_cat, bev_img_gt], axis=1)
+        demo_video.write_frame(img_final)
+    demo_video.close()
